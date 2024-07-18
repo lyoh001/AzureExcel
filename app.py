6373ad3cb36c28511b26c0a7932260712d227715
lyoh001@hotmail.com <lyoh001@hotmail.com>
2024-07-18 13:55:55 +1000

    Commit

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ app.add_middleware(
 downloaded_files_path = "/tmp/downloaded_files.txt"
 patterns = [
     ["CyberArk%20and%20DigiCert", "SOC"],
-    # ["Security%20Tools", "SOC"],
+    ["Security%20Tools", "SOC"],
     # ["SOC%202%20Services", "SOC"],
     # ["Windows", "SOC 2 - Windows Privileged User Access"],
     # ["Windows", "3402 - Windows Privileged User Access"],
