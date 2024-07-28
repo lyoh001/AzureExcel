20c5469a1fa5282efae3e8c28a1bb0bc8eb5ee51
lyoh001@hotmail.com <lyoh001@hotmail.com>
2024-07-28 10:24:09 +1000

    Commit

--- app.py
+++ app.py
@@ -120,28 +120,36 @@ async def download_file_async(
     return temp_file_path
 
 
-async def send_files_to_logic_app(downloaded_files):
-    url = os.environ["LOGIC_APP_URL"]
-    attachments = []
-    for file_index, file_path in enumerate(
-        downloaded_files[: len(downloaded_files) // 2]
-    ):
+async def send_files_to_logic_app(downloaded_files, month):
+    async def send_attachment(file_path, file_index):
         file_name = os.path.basename(file_path)
         folder_name = patterns[file_index][0]
+
         async with aiofiles.open(file_path, "rb") as file:
             content = await file.read()
             encoded_content = base64.b64encode(content).decode("utf-8")
 
-        attachments.append(
-            {"name": file_name, "folder": folder_name, "content": encoded_content}
-        )
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                url=os.environ["LOGIC_APP_URL"],
+                json={
+                    "attachment": {
+                        "name": file_name,
+                        "folder": os.environ["SHAREPOINT_URL"]
+                        + f"%2F{month}%2F{folder_name}",
+                        "content": encoded_content,
+                    }
+                },
+            ) as response:
+                print(f"Logic App response status for {file_name}: {response.status}")
 
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            url=url,
-            json={"attachments": attachments},
-        ) as response:
-            print(f"Logic App response status: {response.status}")
+    tasks = [
+        send_attachment(file_path, file_index)
+        for file_index, file_path in enumerate(
+            downloaded_files[: len(downloaded_files) // 2]
+        )
+    ]
+    await asyncio.gather(*tasks)
 
 
 def save_downloaded_files_to_file(downloaded_files):
@@ -852,6 +860,7 @@ async def update_data(request: Request, data: dict):
         save_downloaded_files_to_file(downloaded_files)
         id = data["data"]["userInfo"].split("@")[0].replace(".", " ").title()
         time = datetime.now().strftime("%d/%m/%Y")
+        month = df["name"].iloc[0].replace(" ", "%20")
         modification_tasks = []
         for file_index, file_path in enumerate(
             downloaded_files[: len(downloaded_files) // 2]
@@ -861,10 +870,9 @@ async def update_data(request: Request, data: dict):
         await asyncio.gather(*modification_tasks)
 
         # Sending Files to Logic App
-        await send_files_to_logic_app(downloaded_files)
+        await send_files_to_logic_app(downloaded_files, month)
 
         # Sending Files to Share Point
-        # month = df["name"].iloc[0].replace(" ", "%20")
         # upload_tasks = []
         # for file_index, file_path in enumerate(
         #     downloaded_files[: len(downloaded_files) // 2]
